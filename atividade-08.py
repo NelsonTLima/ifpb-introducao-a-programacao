9e90d75b0114ba49938a768352b6cb2b1ec6fb75
NelsonTLima <nelsontlima@gmail.com>
2023-05-27 23:05:42 -0300

    add atividade 8.

---
+++ atividade-08.py
@@ -0,0 +1,60 @@
+print("1. Escreva um programa que leia uma frase e a exiba “na vertical”, ou seja, com uma letra em cada linha.\n")
+
+palavra = input("Digite a palavra: ")
+for i in palavra:
+    print(i)
+
+
+print("\n2. Escreva um programa que leia uma frase e determine a quantidade de brancos contidos na mesma.\n")
+
+frase = input("Digite uma frase\n-> ")
+
+c = 0
+for i in frase:
+    if i == " ":
+        c += 1
+print("Sua frase contém", c, "espaço(s) branco(s).")
+
+print("\n3. Escreva um programa que leia uma frase e a exiba invertida.\n")
+
+frase = list(input("Digite uma frase\n-> "))
+
+for i in range((len(frase)//2)):
+    frase[i], frase[len(frase) - i -1] = frase[len(frase) - i - 1], frase[i]
+
+frase = ''.join(frase)
+print(frase)
+
+verbo = input("Digite um verbo: ")
+
+match verbo[-2:]:
+    case "ar":
+        print(
+                f"Eu {verbo[:-2]}o",
+                f"Tu {verbo[:-2]}as",
+                f"Ele {verbo[:-2]}a",
+                f"Nós {verbo[:-2]}amos",
+                f"Vós {verbo[:-2]}ais",
+                f"Eles {verbo[:-2]}am",
+                sep="\n"
+                )
+    case "er":
+        print(
+                f"Eu {verbo[:-2]}o",
+                f"Tu {verbo[:-2]}es",
+                f"Ele {verbo[:-2]}e",
+                f"Nós {verbo[:-2]}emos",
+                f"Vós {verbo[:-2]}eis",
+                f"Eles {verbo[:-2]}em",
+                sep="\n"
+                )
+    case "ir":
+        print(
+                f"Eu {verbo[:-2]}o",
+                f"Tu {verbo[:-2]}es",
+                f"Ele {verbo[:-2]}e",
+                f"Nós {verbo[:-2]}imos",
+                f"Vós {verbo[:-2]}is",
+                f"Eles {verbo[:-2]}em",
+                sep="\n"
+                )
